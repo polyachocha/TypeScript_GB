285494ae6b271831d9ec5825486a94e71a92efe7
Anna Polyakova <polyakova.as@mail.ru>
2022-12-06 20:11:04 +0300

    homework for lesson-2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { renderSearchFormBlock } from './search-form.js'
 import { renderSearchStubBlock } from './search-results.js'
+import { getUserData } from './user.js'
+import { getFavoritesAmount } from './user.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 
+
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('0')
+  getUserData()
+  getFavoritesAmount()
+  renderUserBlock(localStorage.user.username, localStorage.user.avatarUrl, localStorage.favoritesAmount.favoriteItemsAmount)
   renderSearchFormBlock()
   renderSearchStubBlock()
   renderToast(
--- src/user.ts
+++ src/user.ts
@@ -1,23 +1,32 @@
-import { renderBlock } from './lib.js'
+import { renderBlock } from './lib'
 
-export function renderUserBlock (name:'string', avatar:'string', favoriteItemsAmount:number) {
-  const userInfo = {
-    name: 'Wade Warren',
-    avatar: '/img/avatar.png',
-    favoriteItemsAmount: 7
+export function getUserData(value:unknown) {
+  if (value instanceof  ) {
+    const info = JSON.parse(localStorage.getItem('user'))
+    return info
   }
-  const favoritesCaption = (favoriteItemsAmount >= 1) ? true : 'ничего нет'
+  
+}
 
+export function getFavoritesAmount() {
+  const quantity = JSON.parse(localStorage.getItem('favoriteItemsAmount'))
+  return quantity
+}
+
+export function renderUserBlock (userName: string, avatarUrl: string, favoriteItemsAmount?: number) {
+  
+  const favoritesCaption = (favoriteItemsAmount >= 1) ? true : 'ничего нет'
+  const hasFavoriteItems = favoriteItemsAmount ? true : false
 
   renderBlock(
     'user-block',
     `
     <div class="header-container">
-      <img class="avatar" src="${userInfo.avatar}" alt="Wade Warren" />
+      <img class="avatar" src="${avatarUrl}" alt="Wade Warren" />
       <div class="info">
-          <p class="name">${userInfo.name}</p>
+          <p class="name">${userName}</p>
           <p class="fav">
-            <i class="heart-icon${favoritesCaption ? 'active' : ''}"></i>${userInfo.favoriteItemsAmount}
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption.valueOf}
           </p>
       </div>
     </div>
